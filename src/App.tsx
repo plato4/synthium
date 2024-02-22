5ab4d2ef7f4a764373c65f2bcd5464a1fe289698
Dean R <platoisalive@outlook.com>
2024-02-22 04:35:32 +0000

    added first connections

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,66 @@ import "./App.css";
 import * as Tone from "tone";
 
 const createSynth = () => {
+  //LFO
+  const lfoRate = 5;
+  const lfo = new Tone.LFO(lfoRate, -10, 10).start();
+
   //OSC
   const note = 440;
   const wave = "sine";
+  const osc = new Tone.Oscillator(note, wave).start();
 
-  //LFO
-  const lfoRate = 10;
+  //HP
+  const hpFreq = 1500;
+  const hp = new Tone.Filter(hpFreq, "highpass");
+
+  //LP
+  const lpDelay = 0;
+  const lpRes = 0;
+  const lpDamp = 0;
+  const lp = new Tone.LowpassCombFilter(lpDelay, lpRes, lpDamp);
 
   //ENV
-  const attack = 0.0;
-  const decay = 0.2;
-  const sustain = 1.0;
-  const release = 0.8;
-
-  const osc = new Tone.Oscillator(note, wave);
-  const lfo = new Tone.LFO(lfoRate, -10, 10).start();
-
+  const attack = 0;
+  const decay = 0;
+  const sustain = 0;
+  const release = 5;
   const amp = new Tone.AmplitudeEnvelope({
     attack,
     decay,
     sustain,
     release,
-  }).toDestination();
+  });
+
+  //CHORUS
+  const chorusFreq = 15;
+  const chorusDelay = 0.5;
+  const chorusDepth = 5;
+  const chorus = new Tone.Chorus(chorusFreq, chorusDelay, chorusDepth)
+    .toDestination()
+    .start();
 
   lfo.connect(osc.volume);
-  osc.connect(amp).start();
-  return amp;
+  osc.connect(hp);
+  hp.connect(amp);
+  amp.connect(chorus);
+  chorus.toDestination();
+
+  return {
+    start: () => {
+      amp.triggerAttackRelease("8t");
+    },
+  };
 };
 
 function App() {
-  const amp = createSynth();
+  const synth = createSynth();
   Tone.start();
   return (
     <>
       <button
         onClick={() => {
-          amp.triggerAttackRelease("8t");
+          synth.start();
         }}
       ></button>
     </>
